ffca728c48e5a2c6ef83c5b9583742a0bb6fda1f
amycosvn <info@amycos.vn>
2024-09-08 05:17:40 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const allowedImageWordPressDomain = new URL(
 ).hostname;
 
 module.exports = {
-  trailingSlash: true,
+  // trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
@@ -31,6 +31,61 @@ module.exports = {
   // },
   async redirects() {
     return [
+      {
+        source: "/san-pham/tinh-chat-pha-nam-melasma-pell-20-xoa-nam-tan-goc-da-trang-sang-min-mang",
+        destination: "/san-pham/tinh-chat-tri-nam-amycos-melasma-pell-20",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/amycos-glow-booster-serum-duong-da-mo-nam-tan-nhang",
+        destination: "/san-pham/serum-duong-da-cang-bong-amycos-glow-booster",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/kem-tri-nam-melasma-cream-02-phu-hop-moi-loai-da",
+        destination: "/san-pham/kem-duong-trang-da-amycos-melasma-02",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/kem-tri-nam-diamond-melasma-03",
+        destination: "/san-pham/kem-phuc-hoi-da-amycos-diamond-melasma-03",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/sua-rua-mat-perfect-foaming",
+        destination: "/san-pham/sua-rua-mat-tao-bot-amycos-perfect-foaming",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/kem-chong-nang-uv-sun-block-50ml",
+        destination: "/san-pham/kem-chong-nang-amycos-sun-block-cream-spf-50",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/serum-acnes-skin",
+        destination: "/san-pham/tinh-chat-tri-mun-amycos-acnes-skin",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/serum-hoa-huong-duong-amycos",
+        destination: "/san-pham/serum-phuc-hoi-da-amycos-sunflower",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/tinh-chat-pha-nam-melasma-pell-10",
+        destination: "/san-pham/tinh-chat-tri-nam-amycos-melasma-pell-10",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/kem-duong-trang-da-ban-dem-amycos-white-premium-night-cream",
+        destination: "/san-pham/kem-duong-trang-da-ban-dem-amycos-white-premium",
+        permanent: true,
+      },
+      {
+        source: "/san-pham/kem-hap-thu-nam-cho-moi-loai-da-amycos-tri-nam-tan-nhang-hieu-qua-nhanh-chong-an-toan",
+        destination: "/san-pham/kem-duong-tri-nam-amycos-melasma-01",
+        permanent: true,
+      },
       {
         source: "/danh-muc-san-pham/offers/:path*",
         destination: "https://blog.amycos.vn/:path*",
